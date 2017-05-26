cc7b60d729b09c0a36b3c4fa0a3ab6ca97880bc9
Hunter Loftis <hunter@hunterloftis.com>
2017-05-26 19:50:27 -0400

    simplified

--- scripts/tracer.js
+++ scripts/tracer.js
@@ -1,35 +1,31 @@
-const BLACK = new Vector3(0, 0, 0)
-
 class Tracer {
-  constructor ({ canvas, scene, bounces }) {
-    this.scene = scene
-    this.bounces = bounces
+  constructor ({ canvas, scene, bounces, gamma = 2.2}) {
+    Object.assign(this, { scene, bounces, gamma })
     this.width = canvas.width
     this.height = canvas.height
-    this.buffer = new Uint32Array(this.width * this.height * 4);
+    this.buffer = new Uint32Array(this.width * this.height * 4).fill(0)
     this.context = canvas.getContext('2d')
-    this.image = this.context.getImageData(0, 0, this.width, this.height)
-    this.exposures = new Uint32Array(this.width * this.height).fill(0)
+    this.imageData = this.context.getImageData(0, 0, this.width, this.height)
+    this.pixels = this.imageData.data.fill(255)
     this.paths = 0
-    this.pathsPerFrame = this.width * this.height
     this._camera = new Camera({ fov: 40 })
+    this._black = new Vector3()
   }
   expose () {
     const index = this._indexForPath(this.paths)
     const pixel = this._pixelForIndex(index)
     const ray = this._camera.ray(pixel.x, pixel.y, this.width, this.height)
     const rgb = this._trace(ray, this.bounces).array
-    const exposures = ++this.exposures[index]
-    const indexRGBA = index * 4
+    const rgba = index * 4
+    const exposures = ++this.buffer[rgba + 3]
     for (let i = 0; i < 3; i++) {
-      this.buffer[indexRGBA + i] += rgb[i]
-      this.image.data[indexRGBA + i] = this._gamma(this.buffer[indexRGBA + i] / exposures, 2.2)
+      this.buffer[rgba + i] += rgb[i]
+      this.pixels[rgba + i] = this._gamma(this.buffer[rgba + i] / exposures)
     }
-    this.image.data[indexRGBA + 3] = 255
     this.paths++
   }
   draw (debug = false) {
-    this.context.putImageData(this.image, 0, 0)
+    this.context.putImageData(this.imageData, 0, 0)
     if (debug) {
       const index = this._indexForPath(this.paths)
       const pixel = this._pixelForIndex(index)
@@ -37,21 +33,11 @@ class Tracer {
       this.context.fillRect(0, pixel.y, this.width, 1)
     }
   }
-  _setPixel (x, y, color) {
-    const index = (x + (y * this.width)) * 4
-    const exposures = Math.ceil((this.paths + 1) / this.pathsPerFrame)
-    const rgb = color.array
-    for (let i = 0; i < 3; i++) {
-      this.buffer[index + i] += rgb[i]
-      this.image.data[index + i] = this._gamma(this.buffer[index + i] / exposures, 2.2)
-    }
-    this.image.data[index + 3] = 255
-  }
-  _gamma(brightness, g) {
-    return Math.pow(brightness / 255, (1 / g)) * 255
+  _gamma (brightness) {
+    return Math.pow(brightness / 255, (1 / this.gamma)) * 255
   }
   _indexForPath (path) {
-    return path % this.pathsPerFrame
+    return path % (this.width * this.height)
   }
   _pixelForIndex (index) {
     const x = index % this.width
@@ -79,6 +65,6 @@ class Tracer {
         return this._trace(refractedRay, bounces - 1, attenuation).scaledBy(gain)
       }
     }
-    return BLACK
+    return this._black
   }
 }
