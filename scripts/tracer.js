62e979ae2a19635ee183fb8265ef57b062a0325f
Hunter Loftis <hunter@hunterloftis.com>
2017-05-30 13:30:30 -0400

    tone mapping happens in one place

--- scripts/tracer.js
+++ scripts/tracer.js
@@ -3,7 +3,8 @@ class Tracer {
     Object.assign(this, { scene, bounces, gamma })
     this.width = canvas.width
     this.height = canvas.height
-    this.buffer = new Uint32Array(this.width * this.height * 4).fill(0)
+    // TODO: Float32Array or UInt32Array?
+    this.buffer = new Float32Array(this.width * this.height * 4).fill(0)
     this.context = canvas.getContext('2d')
     this.imageData = this.context.getImageData(0, 0, this.width, this.height)
     this.pixels = this.imageData.data.fill(255)
@@ -12,6 +13,7 @@ class Tracer {
     this._black = new Vector3()
     this._lastPixel = { x: 0, y: 0 }
     this.context.fillStyle = '#fff'
+    this._gamma = this._gamma.bind(this)
   }
   exposeRandom() {
     const x = Math.floor(Math.random() * this.width)
@@ -29,16 +31,22 @@ class Tracer {
     const rgb = this._trace(ray, this.bounces).array
     const index = (pixel.x + pixel.y * this.width) * 4
     const exposures = ++this.buffer[index + 3]
+    const average = new Float32Array(3)
     for (let i = 0; i < 3; i++) {
       this.buffer[index + i] += rgb[i]
-      this.pixels[index + i] = this._gamma(this.buffer[index + i] / exposures)
+      average[i] = this.buffer[index + i] / exposures
     }
+    this.pixels.set(this._mapped(average), index)
     this.paths++
     this._lastPixel = pixel
   }
   draw (debug = false) {
     this.context.putImageData(this.imageData, 0, 0)
     if (debug) this.context.fillRect(0, this._lastPixel.y, this.width, 1)
+  }
+  _mapped (rgb) {
+    // TODO: HDR tonemapping here (exposure mapping?)
+    return rgb.map(this._gamma)
   }
   _gamma (brightness) {
     return Math.pow(brightness / 255, (1 / this.gamma)) * 255
@@ -51,6 +59,7 @@ class Tracer {
     const { hit, normal, material } = this.scene.intersect(ray)
     if (!hit) return this.scene.background(ray).scaledBy(gain)
 
+    // TODO: take length of ray into account (and reduce energy by that amount here, instead of in bsdf - first step towards participating media / volumetric fog)
     if (!ray.direction.enters(normal)) {
       const direction = ray.direction.refracted(normal.scaledBy(-1), material.refraction, 1)
       const refractedRay = new Ray3(hit, direction)
