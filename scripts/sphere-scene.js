60f5f0b6f4d685983b6e19fd9e39fb28bf4d3864
Hunter Loftis <hunter@hunterloftis.com>
2017-06-01 14:55:50 -0400

    no blue ball

--- scripts/sphere-scene.js
+++ scripts/sphere-scene.js
@@ -5,7 +5,7 @@ class SphereScene extends Scene {
     this.environment = 'images/stpeters-probe.png'
     this.objects = [
       new Sphere(new Vector3(-3.3, 1, -4.3), 1, materials.gold),
-      new Sphere(new Vector3(-1.1, 1, -5), 1, materials.bluePlastic),
+      // new Sphere(new Vector3(-1.1, 1, -5), 1, materials.bluePlastic),
       new Sphere(new Vector3(1, 1, -5), 1, materials.silver),
       new Sphere(new Vector3(3.2, 1, -4.6), 1, materials.greenGlass),
       new Sphere(new Vector3(0.5, -1000, -8), 1000, materials.whiteLambert),
